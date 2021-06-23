c9ddaa9c89fcea03aa2ed00c41364ab5a372902f
RafidMuhymin <rafidmuhymin@gmail.com>
2021-06-23 13:26:50 +0600

    experimental

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,1 @@
 require("./src/styles/prismjs-a11y.css");
-
-// exports.onClientEntry = async () => {
-//   // eslint-disable-next-line no-undef
-//   const res = await fetch(`${__PATH_PREFIX__}/search_index.json`).catch((err) =>
-//     console.log("Failed fetch search index", err)
-//   );
-//   const fullIndex = await res.json();
-//   window.__LUNR__ = {
-//     index: require("elasticlunr").Index.load(fullIndex.index),
-//     store: fullIndex.store,
-//   };
-// };
